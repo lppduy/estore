129d75ba74915e23cc3a802976de653f521a2db1
lppduy <lephamphuongduy@gmail.com>
2024-04-06 14:47:13 +0700

    feat: category dropdown

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ function App() {
       <div className="header bg-dark">
         <div className="row">
           <div className='brand my-1'>
-            <h1>eStore</h1>s
+            <h1>eStore</h1>
           </div>
           <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
+            <div className="dropdown m-0 p-0">
+              <select className='select-btn w-100 p-0 m-0'>
+                <option>Men</option>
+                <option>Women</option>
+                <option>Kids</option>
+              </select>
+            </div>
             <input className='form-control' placeholder='Search...' />
             <button> <i className="fa fa-search"></i></button>
           </div>
