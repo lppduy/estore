a821373966a3ea4bcf502164103b22b0578ca753
lppduy <lephamphuongduy@gmail.com>
2024-04-06 09:16:52 +0700

    feat: init project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header bg-dark">
+        <div className="row">
+          <div className='brand my-1'>
+            <h1>eStore</h1>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
