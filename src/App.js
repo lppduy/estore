5cd37c5295d88cb0e373d1be9e704537475e2c3f
lppduy <lephamphuongduy@gmail.com>
2024-04-06 10:39:40 +0700

    feat: add search bar with icon

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ function App() {
       <div className="header bg-dark">
         <div className="row">
           <div className='brand my-1'>
-            <h1>eStore</h1>
+            <h1>eStore</h1>s
+          </div>
+          <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
+            <input className='form-control' placeholder='Search...' />
+            <button> <i className="fa fa-search"></i></button>
           </div>
         </div>
-
       </div>
     </div>
   );
